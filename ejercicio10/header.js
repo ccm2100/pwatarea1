dedb08db036e7a36a4ea901167418dfb424f23bd
ccm2100 <ccm2100@gmail.com>
2023-10-30 20:25:32 -0500

    se aumentó el ejercicio 10

---
+++ ejercicio10/header.js
@@ -0,0 +1,16 @@
+document.getElementById("header-placeholder").innerHTML =
+    '<header>' +
+    '<nav>' +
+    '<ul>' +
+    '<li><a href="index.html">Inicio</a></li>' +
+    '<li><a href="contacto.html">Contacto</a></li>' +
+    '<li><a href="descripcion.html">Descripción</a></li>' +
+    '<li><a href="portafolio.html">Portafolio</a></li>' +
+    '<li><a href="testimoniales.html">Testimoniales</a></li>' +
+    '<li><a href="noticias.html">Noticias</a></li>' +
+    '<li><a href="precios.html">Precios</a></li>' +
+    '<li><a href="recursos.html">Recursos</a></li>' +
+    '<li><a href="faq.html">Preguntas Frecuentes</a></li>' +
+    '</ul>' +
+    '</nav>' +
+    '</header>';
